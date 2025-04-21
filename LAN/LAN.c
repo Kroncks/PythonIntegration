a3e48f7c1dbd7a6bf493d9109a0ba1dd2ef1ccb7
Kroncks <161207379+Kroncks@users.noreply.github.com>
2025-04-21 19:16:58 +0200

    multiplat

--- LAN/LAN.c
+++ LAN/LAN.c
@@ -1,29 +1,33 @@
 #include "LAN.h"
+#include "crossplatform.h"
 
 
-void client(char * username) {
+
+void client(char *username) {
     char buffer[128];
     printf("[Client C] Lancement de la détection du serveur via client.py...\n");
 
-    FILE *fp = popen("../.venv/bin/python3 ../LAN/client.py", "r");
-    if (fp == NULL) {
+    FILE *fp = popen("python ../LAN/client.py", "r");
+    if (!fp) {
         perror("[Client C] Erreur appel script Python");
         return;
     }
 
     if (fgets(buffer, sizeof(buffer), fp) == NULL) {
-        printf("[Client C] Échec de la détection du serveur (aucun retour de client.py).\n");
+        printf("[Client C] Échec de la détection du serveur.\n");
         pclose(fp);
         return;
     }
     pclose(fp);
     buffer[strcspn(buffer, "\n")] = 0;
-
     printf("[Client C] Serveur détecté à l'adresse : %s\n", buffer);
 
-    // Connexion TCP
-    printf("[Client C] Création de la socket...\n");
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (network_init() != 0) {
+        printf("[Client C] Erreur d'initialisation réseau.\n");
+        return;
+    }
+
+    socket_t sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("[Client C] Erreur création socket");
         return;
@@ -32,53 +36,29 @@ void client(char * username) {
     struct sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(TCP_PORT);
+#ifdef _WIN32
+    serv_addr.sin_addr.s_addr = inet_addr(buffer);
+#else
     if (inet_pton(AF_INET, buffer, &serv_addr.sin_addr) <= 0) {
-        perror("[Client C] Adresse invalide ou conversion échouée");
-        close(sock);
+        perror("[Client C] Adresse invalide");
+        socket_close(sock);
         return;
     }
+#endif
 
-    printf("[Client C] Tentative de connexion au serveur...\n");
+    printf("[Client C] Tentative de connexion...\n");
     if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
         perror("[Client C] Connexion échouée");
-        close(sock);
+        socket_close(sock);
+        network_cleanup();
         return;
     }
 
-    printf("[Client C] Connecté avec succès. Attente d'un message...\n");
-    char recv_msg[128];
-    int n = recv(sock, recv_msg, sizeof(recv_msg)-1, 0);
-    if (n > 0) {
-        recv_msg[n] = '\0';
-        printf("[Client C] Message du serveur : %s\n", recv_msg);
-        if (strncmp(recv_msg, "ID:", 3) == 0) {
-            printf("[Client C] Numéro attribué : %s\n", recv_msg + 3);
-        } else {
-            printf("[Client C] Le serveur a refusé la connexion.\n");
-            close(sock);
-            return;
-        }
-    } else {
-        perror("[Client C] Erreur à la réception du message");
-        close(sock);
-        return;
-    }
+    printf("[Client C] Connecté. Envoi du pseudo : %s\n", username);
+    send(sock, username, strlen(username), 0);
 
-    printf("[Client C] Envoi du pseudo : %s\n", username);
-    if (send(sock, username, strlen(username), 0) < 0) {
-        perror("[Client C] Erreur lors de l'envoi du pseudo");
-    }
-
-    printf("[Client C] Connexion terminée. Appuyez sur Entrée pour fermer.\n");
+    printf("[Client C] Terminé. Appuyez sur Entrée.\n");
     getchar();
-    close(sock);
-}
-
-
-void serveur() {
-    printf("[Serveur] Lancement du serveur Python...\n");
-    int result = system("../.venv/bin/python3 ../LAN/serveur.py");
-    if (result != 0) {
-        printf("[Serveur] Erreur lors du lancement du serveur Python.\n");
-    }
+    socket_close(sock);
+    network_cleanup();
 }
--- LAN/LAN.h
+++ LAN/LAN.h
@@ -5,15 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifdef _WIN32
-    #include <winsock2.h>
-    #include <ws2tcpip.h>
-    #pragma comment(lib, "Ws2_32.lib")
-#else
-    #include <arpa/inet.h>
-    #include <unistd.h>
-#endif
-
 #define TCP_PORT 12345
 
 void client(char * username);
