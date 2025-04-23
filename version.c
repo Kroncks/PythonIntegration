250b24631523fd9f013a19e08b91d0fd952951e8
Kroncks <161207379+Kroncks@users.noreply.github.com>
2025-04-23 15:05:23 +0200

    version

--- version.c
+++ version.c
@@ -3,13 +3,15 @@
 #define LEN 20
 #define FILENAME "../version.txt"
 
+#define PRGRM_NAME "CLM"
+
 void version() {
     char version_n[LEN];
     FILE *file = fopen(FILENAME, "r");
     if (fgets(version_n, LEN, file) != NULL) {
         // Enlève le saut de ligne s'il y en a un
         version_n[strcspn(version_n, "\n")] = '\0';
-        printf("Version : %s\n", version_n);
+        printf("%s %s\n",PRGRM_NAME, version_n);
     } else {
         printf("Erreur lors de la lecture de la version.\n");
     }
