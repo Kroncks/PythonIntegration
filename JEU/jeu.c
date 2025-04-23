15fae94d7e1ddca2376c63c6e17abba2b821d69a
Kroncks <161207379+Kroncks@users.noreply.github.com>
2025-04-23 12:51:11 +0200

    init coo

--- JEU/jeu.c
+++ JEU/jeu.c
@@ -51,6 +51,9 @@ void init_game(socket_t sock, Game * game, int num, char * username) {
             }
         }
     }
+    //coordonées de base des joueurs :
+    game->players[0].x = 0; game->players[0].y = Y/2;
+    game->players[1].x = X; game->players[1].y = Y/2;
 }
 
 
