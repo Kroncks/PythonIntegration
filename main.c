1523fc2d162abd7c75d70c72a37acb076221e802
Kroncks <161207379+Kroncks@users.noreply.github.com>
2025-05-02 16:35:14 +0200

    hmm

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main() {
         do {
             printf("\n=======[ MENU ]=======\n 0) Partie locale\n 1) Creer une LAN\n 2) Rejoindre une LAN\n 3) Quitter\n======================\n[X]==>");
             scanf("%d", &choix);  printf("\n");
-        } while (choix<1 || choix>3);
+        } while (choix<0 || choix>3);
         switch (choix) {
             case 0: {
                 // il faudrait pouvoir connaitre le nombre de joueur en ammont
